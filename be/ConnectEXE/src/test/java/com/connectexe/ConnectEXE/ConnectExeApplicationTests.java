bbfa2c52d7559f93a005a1041142d7e15f3ebf53
anlttce182383 <anlttce182383@fpt.edu.vn>
2025-09-24 15:09:03 +0700

    Adding deafault project

---
+++ be/ConnectEXE/src/test/java/com/connectexe/ConnectEXE/ConnectExeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connectexe.ConnectEXE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectExeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
